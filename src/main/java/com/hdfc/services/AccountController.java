62170c36cbe8b1219201f223460bbd5645a7f898
Sandeep_Sable <tech.ssable@gmail.com>
2022-09-06 19:45:47 +0530

    test

--- src/main/java/com/hdfc/services/AccountController.java
+++ src/main/java/com/hdfc/services/AccountController.java
@@ -4,12 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LoanController {
- 
-	
-	@GetMapping("/loan")
-	public String getStudentInfo()
-	{
-       return  "this is test loan service. its up and running";		
+public class AccountController {
+
+	@GetMapping("/account")
+	public String getStudentInfo() {
+		return "this is test account service. its up and running.sns";
 	}
 }
